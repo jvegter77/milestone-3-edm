67afd04f7ad35b8467d7affc7735dee072178298
Jeroen Vegter <jeroenvegter@hotmail.com>
2019-03-27 16:01:26 +0000

    connected to mongo db and created first landing page for application

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'milestone3_edm'
+app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster-gdxlz.mongodb.net/milestone3_edm?retryWrites=true'
+
+mongo = PyMongo(app)
+
+
+
 @app.route('/')
-def hello():
-    return 'Hello World ...again!'
+@app.route('/get_festivals')
+def get_festivals():
+    return render_template("festivals.html", festivals=mongo.db.festivals.find())
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
