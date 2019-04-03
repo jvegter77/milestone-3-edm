c1b2f982a86ac1ac09499135d23f96de1d95cb4f
Jeroen Vegter <jeroenvegter@hotmail.com>
2019-04-03 16:25:09 +0000

    added page to edit a festival and created basic route in flask

--- app.py
+++ app.py
@@ -10,15 +10,13 @@ app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster-gdxlz.mong
 
 mongo = PyMongo(app)
 
-
-
 @app.route('/')
 def get_index():
     return render_template('index.html')
 
 @app.route('/get_festivals')
 def get_festivals():
-    return render_template("festivals.html", festivals=mongo.db.festivals.find())
+    return render_template('festivals.html', festivals=mongo.db.festivals.find())
     
 @app.route('/add_festival')
 def add_festival():
@@ -29,7 +27,14 @@ def insert_festival():
     festivals = mongo.db.festivals
     festivals.insert_one(request.form.to_dict())
     return redirect(url_for('get_festivals'))
-    
+
+@app.route('/edit_festival/<festivals_id>')
+def edit_festival(festivals_id):
+    festivals = mongo.db.festivals.find_one({'_id': ObjectId(festivals_id)})
+    all_festivals = mongo.db.festivals.find()
+    return render_template('editfestival.html')
+      
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
         port=int(os.environ.get('PORT')),
