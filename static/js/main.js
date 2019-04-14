1da574d6b92140d3a8e356988533f4258f441fe4
Jeroen Vegter <jeroenvegter@hotmail.com>
2019-04-14 10:40:56 +0000

    added js for the calendar function and sort method for view festivals

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,8 @@
 document.getElementById('navbartoggler').onclick = function(event) {
    document.getElementById('mobilenav').className = "show";
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.datepicker');
+    var instances = M.Datepicker.init(elems, open);
+  });
